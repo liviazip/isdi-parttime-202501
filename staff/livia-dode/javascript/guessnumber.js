cbd16e98c9bfb37a962be6efa8344cf59af38618
unknown <welelemillian23@gmail.com>
2025-02-02 23:09:54 +0100

    add practice guessnumber game

--- staff/livia-dode/javascript/guessnumber.js
+++ staff/livia-dode/javascript/guessnumber.js
@@ -1,7 +1,36 @@
-var str = "a";
-var str1 = "b";
+var isGameOn = confirm('¿Quieres jugar a un juego?');
 
+if (isGameOn) {
+    guessNumber();
+} else {
+    alert('Hasta la próxima!');
+}
 
-var num = 1;
-var numInt = 9231;
-var numFloat = 1,4;
+function guessNumber() {
+	var minNumber = 1;
+	var maxNumber = 100;
+    var secretNumber = Math.floor(Math.random() * maxNumber - minNumber + 1) + minNumber; 
+    var playerGuess = null;
+    var attempts = 0; 
+ 
+while (playerGuess !== secretNumber) {
+        var input = prompt('¿En qué número crees que estoy pensando?');      
+        if (input === null) {
+            alert('Juego cancelado. Bye!');
+            return;
+        }
+
+        playerGuess = Number(input);
+        attempts++;
+
+ 		if (isNaN(playerGuess)) {
+            alert('Por favor, inserta un número válido.');
+        } else if (playerGuess < secretNumber) {
+            alert('Demasiado bajo, intenta otra vez.');
+        } else if (playerGuess > secretNumber) {
+            alert('Demasiado alto, intenta otra vez.');
+        } else {
+            alert(´Enhorabuena! Has acertado en ${attempts} intentos.´);
+        }
+    }
+}
